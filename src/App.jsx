9f50dc9c2b240d5b1116d2a9e21e3747e453f5a8
Erich <eebbinghaus@gmail.com>
2023-01-22 20:46:11 -0700

    changed img url

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,49 @@
 import {
-  Navbar,
-  About,
-  Contact,
-  Footer,
-  Portfolio,
-  Resume,
-  Home,
+    Navbar,
+    About,
+    Contact,
+    Footer,
+    Portfolio,
+    Resume,
+    Home,
 } from "./components";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-
-
 function App() {
-  return (
-    <div className="bg-[url('./src/assets/bgGrey.png')] w-full md:h-screen overflow-hidden bg-no-repeat bg-cover ">
-      <Router>
-        <div className="">
-          <div>
-            <Navbar />
-            <Routes>
-              <Route exact path="/react-portfolio" element={<Home />} />
-              <Route exact path="/about" element={<About />} />
-              <Route exact path="/portfolio" element={<Portfolio />} />
-              <Route exact path="/contact" element={<Contact />} />
-              <Route exact path="/resume" element={<Resume />} />
-            </Routes>
-            <Footer />
-          </div>
+    return (
+        <div
+            className=" w-full md:h-screen overflow-hidden bg-no-repeat bg-cover "
+            style={{ backgroundImage: "url(./src/assets/bgGrey.png)" }}
+        >
+            <Router>
+                <div className="">
+                    <div>
+                        <Navbar />
+                        <Routes>
+                            <Route
+                                exact
+                                path="/react-portfolio"
+                                element={<Home />}
+                            />
+                            <Route exact path="/about" element={<About />} />
+                            <Route
+                                exact
+                                path="/portfolio"
+                                element={<Portfolio />}
+                            />
+                            <Route
+                                exact
+                                path="/contact"
+                                element={<Contact />}
+                            />
+                            <Route exact path="/resume" element={<Resume />} />
+                        </Routes>
+                        <Footer />
+                    </div>
+                </div>
+            </Router>
         </div>
-      </Router>
-    </div>
-  );
+    );
 }
 
 export default App;
